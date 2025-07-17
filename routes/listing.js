523439b7fb3feb4b591e899302d5dc2db2bb2c13
CyberHunter8857 <mayurtamanke7744@gmail.com>
2025-07-17 19:24:34 +0530

    Added Authentication and Authorization.

--- routes/listing.js
+++ routes/listing.js
@@ -4,17 +4,9 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
-const { isLoggedIn } = require("../middleware.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+
 
 //Index Routing
 router.get(
@@ -38,7 +30,12 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id)
-      .populate("reviews")
+      .populate({
+        path:"reviews",
+        populate:{
+          path:"author"
+        },
+      })
       .populate("owner");
     console.log(listing);
     res.render("listings/show.ejs", { listing });
@@ -52,7 +49,7 @@ router.post(
   validateListing,
   wrapAsync(async (req, res, next) => {
     let result = listingSchema.validate(req.body);
-    console.log(result);
+    console.log(req.user);
     if (result.error) {
       throw new ExpressError(400, result.error);
     }
@@ -70,6 +67,7 @@ router.post(
 router.get(
   "/:id/edit",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -81,6 +79,7 @@ router.get(
 router.put(
   "/:id",
   isLoggedIn,
+  isOwner,
   validateListing,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
@@ -94,6 +93,7 @@ router.put(
 router.delete(
   "/:id",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
